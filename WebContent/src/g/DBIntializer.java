c7abc90a1a817ee03dc446f600ca6f61132cbb8e
unknown <aditiapte3611@gmail.com>
2022-02-03 12:44:58 +0530

    this is servlet project

---
+++ WebContent/src/g/DBIntializer.java
@@ -0,0 +1,8 @@
+package g;
+
+public interface DBIntializer {
+String DRIVER="oracle.jdbc.driver.OracleDriver";
+String CON_STRING="jdbc:oracle:thin:@localhost:1521:orcl";
+String USERNAME="sys as sysdba";
+String PASSWORD="";
+}
